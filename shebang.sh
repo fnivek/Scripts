6a35e96e234a88101082ecf241d88c7ede469243
Kevin French <fnivek@gmail.com>
2017-01-16 10:40:26 -0500

    SHEBANG: Setup shebang for sh and bash scripts

--- shebang.sh
+++ shebang.sh
@@ -1,16 +1,54 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-	echo 'Please pass a file as the only argument' >&2
-	exit 1
-fi 
+print_usage () {
+	printf "Usage: $0 [options] file\n\t-p python\n\t-s sh\n\t-b bash\n\t-h help\n"
+	exit 0
+}
 
-python_shebang="#!/usr/bin/env python"
-num_lines="$(cat $1 | wc -l)" 2> /dev/null
+# Parse options
+while getopts :psb opt; do
+	case $opt in
+		p)
+			p=${OPTARG}
+		;;
+		s)
+			s=${OPTARG}
+		;;
+		b)
+			b=${OPTARG}
+		;;
+		*)
+			print_usage
+		;;
+	esac
+done
+
+# Parse file
+if [[ $(($# - $OPTIND)) -lt 0 ]]; then
+	# No file passed
+	echo "No file passed"
+	print_usage
+fi
+
+f=${@:OPTIND:1}
+
+if [ ! -f $f ]; then
+	echo "$f is not a file"
+	print_usage
+fi
+
+shebang="#!/usr/bin/env python"
+if ! [[ -z ${s+x} ]]; then
+	shebang="#!/bin/sh"
+elif ! [[ -z ${b+x} ]]; then
+	shebang="#!/bin/bash"
+fi
+
+num_lines="$(cat $f | wc -l)" 2> /dev/null
 
 if [ ${num_lines} -eq 0 ]; then
-	echo ${python_shebang} >> $1
+	echo ${shebang} >> $f
 	exit 0
 fi
 
-sed -i "1 i${python_shebang}" $1
+sed -i "1 i${shebang}" $f
